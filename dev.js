e5394df512347d33e8529fabb8460311e816db76
Unyeong Baek <eunnamu094@gmail.com>
2020-01-22 19:01:36 +0900

    greenlock

--- dev.js
+++ dev.js
@@ -1,54 +1,44 @@
-require('./src/Clan')
-require('./src/Item')
-require('./src/Room')
 require('./src/User')
+require('./src/Room')
+require('./src/Clan')
 
-const lex = require('greenlock-koa').create({
-    version: 'draft-11',
-    configDir: '/etc/letsencrypts',
-    server: 'https://acme-staging-v02.api.letsencrypt.org/directory',
-    approveDomains: (opts, certs, cb) => {
-        if (certs) {
-            opts.domains = ['sandspoon.com']
-        } else {
-            opts.email = 'sandspoons@gmail.com'
-            opts.agreeTos = true
-        }
-        cb(null, { options: opts, certs })
-    },
-    communityMember: true,
-    configDir: require('os').homedir() + '/acme/etc',
-    renewWithin: 81 * 24 * 60 * 60 * 1000,
-    renewBy: 80 * 24 * 60 * 60 * 1000
-})
-
-const https = require('https')
+const http = require('http')
 const Koa = require('koa')
 const Router = require('koa-router')
 const bodyparser = require('koa-bodyparser')
-const Data = require('./src/Data')
-const DB = require('./src/DB')
 const Server = require('./src/Server')
-const auth = require('./src/api/auth')
-const androidpublisher = require('./src/api/androidpublisher')
+const auth = require('./src/auth')
 const app = new Koa()
 const router = new Router()
+const Data = require('./src/Data')
+const DB = require('./src/DB')
 
-const VERSION = '0.2.1'
+const VERSION = '0.2.2'
 const PORT = 50000
 
-router.get('/', ctx => {
+router.get('/', (ctx, next) => {
     ctx.body = VERSION
+})
+
+router.get('/terms', (ctx, next) => {
+    ctx.body = "이용약관"
 })
 
 app.use(bodyparser())
     .use(router.routes())
     .use(auth.routes())
-    .use(androidpublisher.routes())
 
 const Logger = require('./Logger')
 global.logger = new Logger('[INFO]', true)
 
+async function test() {
+    try {
+        throw new Error('hihi')
+    } catch (e) {
+        console.error(e)
+        throw e
+    }
+}
 async function start() {
     try {
         await Data.loadData()
@@ -64,11 +54,10 @@ async function start() {
                 Data.loadRanks()
             }
         }, 1000 * 300)
-        https.createServer(lex.httpsOptions, lex.middleware(app.callback())).listen(443)
+        http.createServer().listen(80)
         new Server().run(PORT)
         console.log('server is running.')
     } catch (e) {
-        console.log('A')
         console.log(e)
     }
 }
@@ -86,6 +75,7 @@ process.on('SIGINT', async () => {
     logger.writeFile('./log.txt')
     console.log('Caught interrupt signal.')
     process.exit()
+
 })
 
 start()
