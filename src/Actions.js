241865ce1515ff85a0d3307d71c3f13eb8020d87
Unyeong Baek <eunnamu094@gmail.com>
2020-02-28 21:29:54 +0900

    fix bug

--- src/Actions.js
+++ src/Actions.js
@@ -379,7 +379,8 @@ class EscapeState {
             for (const red of mode.redTeam) {
                 if (red.place === context.place) {
                     const range = Math.abs(red.x - context.x) + Math.abs(red.y - context.y)
-                    if (range >= 12) continue
+                    if (range >= 12)
+                        continue
                     if (red.game.hp < 0) {
                         mode.moveToBase(red)
                         red.game.hp = 100
