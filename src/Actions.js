7d7488146ee29fa9b8c3a02532122271b95236fd
Unyeong Baek <eunnamu094@gmail.com>
2020-02-28 21:25:23 +0900

    fix bug

--- src/Actions.js
+++ src/Actions.js
@@ -379,13 +379,13 @@ class EscapeState {
             for (const red of mode.redTeam) {
                 if (red.place === context.place) {
                     const range = Math.abs(red.x - context.x) + Math.abs(red.y - context.y)
-                    if (range > 2) continue
+                    if (range >= 5) continue
                     if (red.game.hp < 0) {
                         mode.moveToBase(red)
                         red.game.hp = 100
                         red.send(Serialize.InformMessage('<color=red>탈출구에서 벗어나지 않아 강제로 추방되었습니다.</color>'))
                     } else {
-                        red.game.hp -= 40
+                        red.game.hp -= 50
                         red.send(Serialize.InformMessage('<color=red>탈출구에서 벗어나세요!!!!</color>'))
                         red.send(Serialize.PlaySound('Warn'))
                     }
