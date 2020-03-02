ad9f78b2402ee48a91185ca93a8f3a0070eb2a2f
Unyeong Baek <eunnamu094@gmail.com>
2020-03-03 01:34:19 +0900

    fix bug

--- src/Methods.js
+++ src/Methods.js
@@ -18,20 +18,20 @@ class FireMethod {
         for (const red of room.mode.redTeam) {
             if (red.place === self.place) {
                 const range = Math.abs(red.x - self.x) + Math.abs(red.y - self.y)
-                if (range > 2)
-                    continue
-                if (red.game.hp < 0) {
-                    room.mode.moveToKickOut(red)
-                    red.game.hp = 100
-                    ++room.mode.score.blue
-                    ++self.score.kill
-                    ++red.score.death
-                    red.send(Serialize.InformMessage('<color=red>사망했습니다.</color>'))
-                    self.send(Serialize.InformMessage('<color=red>' + red.name + ' 소탕 완료!</color>'))
-                    self.publish(Serialize.UpdateModeCount(room.mode.score.red, room.mode.score.blue))
-                } else {
-                    red.game.hp -= parseInt(Math.random() * 60) + 40
-                    self.publishToMap(Serialize.SetAnimation(red, 'Fire', 'Fire'))
+                if (range <= 2) {
+                    if (red.game.hp < 0) {
+                        room.mode.moveToKickOut(red)
+                        red.game.hp = 100
+                        ++room.mode.score.blue
+                        ++self.score.kill
+                        ++red.score.death
+                        red.send(Serialize.InformMessage('<color=red>사망했습니다.</color>'))
+                        self.send(Serialize.InformMessage('<color=red>' + red.name + ' 소탕 완료!</color>'))
+                        self.publish(Serialize.UpdateModeCount(room.mode.score.red, room.mode.score.blue))
+                    } else {
+                        red.game.hp -= parseInt(Math.random() * 70) + 30
+                        self.publishToMap(Serialize.SetAnimation(red, 'Fire', 'Fire'))
+                    }
                 }
             }
         }
